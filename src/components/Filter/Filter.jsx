e8f73592f8cd77bd8bdfb5b166ed514f8c35a5ab
Sergei-Bug <122543402+Sergei-Bug@users.noreply.github.com>
2023-10-30 17:56:11 +0200

    HW

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter } from 'redux/filterSlice';
+import { getFilter } from 'redux/selectors';
+
+export function Filter() {
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        name="filter"
+        value={useSelector(getFilter)}
+        onChange={e => {
+          dispatch(changeFilter(e.target.value));
+        }}
+      />
+    </>
+  );
+}
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const defContacts = [];
+
+export const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: { contacts: defContacts },
+  reducers: {
+    addContact(state, action) {
+      state.contacts.push(action.payload);
+    },
+    deleteContact(state, action) {
+      const index = state.contacts.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.contacts.splice(index, 1);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
